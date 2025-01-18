b3b52f0827d6217d86723f5ea184316353c401fb
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 13:58:33 +0000

    Create task file

---
+++ index.js
@@ -0,0 +1,61 @@
+class Node {
+    constructor(value) {
+       
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        
+    }
+
+    // Insert a value into the BST
+    insert(value) {
+        
+    }
+
+    // Helper function to insert a node
+    insertNode(node, newNode) {
+        
+    }
+
+    // Search for a node by its value
+    search(value) {
+       
+    }
+
+    // Helper function to search for a node
+    searchNode(node, value) {
+        
+    }
+
+    // Delete a node by its value
+    delete(value) {
+       
+    }
+
+    // Helper function to delete a node
+    deleteNode(node, value) {
+        
+    }
+
+    // Helper function to find the minimum node
+    minNode(node) {
+       
+    }
+
+    // In-order traversal (left, root, right)
+    inOrder() {
+       
+    }
+
+    // Helper function for in-order traversal
+    inOrderTraverseNode(node) {
+        
+    }
+
+    // Get the size of the BST
+    size() {
+        
+    }
+}
